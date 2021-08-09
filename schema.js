25515a902770ad67a0bc42b5b5ba0f90573c44a3
MJ <manabshy@gmail.com>
2021-08-09 12:39:56 +0100

    Speaker API

--- schema.js
+++ schema.js
@@ -16,6 +16,14 @@ type Query {
   
   ):[Session],
   sessionById(id: ID): Session
+  speakers: [Speaker]
+  speakerById(id: ID): Speaker
+}
+type Speaker {
+    id: ID!
+    bio: String
+    name: String
+    sessions: [Session]
 }
 type Session {
   id: ID!
