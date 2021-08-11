89d0fdb61ee991470bcccfb6890e2ffb50e98bf4
MJ <manabshy@gmail.com>
2021-08-11 22:21:27 +0100

    nesting resolvers

--- schema.js
+++ schema.js
@@ -1,39 +1,43 @@
 const { gql } = require('apollo-server');
-module.exports = 
-gql`
-type Query {
-  sessions(
-    id: ID
-    title: String,
-    description:String,
-    startsAt:String,
-    endsAt:String,
-    room:String,
-    day:String,
-    format: String,
-    track:String,
-    level:String
-  
-  ):[Session],
-  sessionById(id: ID): Session
-  speakers: [Speaker]
-  speakerById(id: ID): Speaker
-}
-type Speaker {
+
+module.exports = gql`
+  type Query {
+    sessions(
+      id: ID
+      title: String
+      description: String
+      startsAt: String
+      endsAt: String
+      room: String
+      day: String
+      format: String
+      track: String
+      level: String
+    ): [Session]
+    sessionById(id: ID): Session
+    speakers: [Speaker]
+    speakerById(id: ID): Speaker
+  }
+  type Speaker {
     id: ID!
     bio: String
     name: String
     sessions: [Session]
-}
-type Session {
-  id: ID!
-  title: String!,
-  description:String,
-  startsAt:String,
-  endsAt:String,
-  room:String,
-  day:String,
-  format: String,
-  track:String @deprecated(reason: "OK, not in use anymore"),
-  level:String
-}`
+  }
+  type Session {
+    id: ID!
+    title: String!
+    description: String
+    startsAt: String
+    endsAt: String
+    room: String
+    day: String
+    format: String
+    track: String
+      @deprecated(
+        reason: "Too many sessions do not fit into a single track, we will be migrating to a tags based system in the future..."
+      )
+    level: String
+    speakers: [Speaker]
+  }
+`;
